38766052682cb93651a0baa289be86ae6fd0855e
rishabbaba17@gmail.com <rishabbaba10@gmail.com>
2019-05-19 04:57:10 +0530

    Updates

---
+++ precache-manifest.bbbc12ae6bbdfb16abe7a8a98fc25d62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7122fb9e00ab3ae50f9f41bc39f23b17",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "a474c1ecc02e46809d89",
+    "url": "/rock-paper-scissors/static/css/main.83514c37.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/rock-paper-scissors/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a474c1ecc02e46809d89",
+    "url": "/rock-paper-scissors/static/js/main.c5fc1a95.chunk.js"
+  },
+  {
+    "revision": "cf8ffcfd822dc753bb49",
+    "url": "/rock-paper-scissors/static/js/runtime~main.c4e15f11.js"
+  }
+]);
